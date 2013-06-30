c728854206d316a4943f6cee96bdcb59ea70919a
xeoron <aitsinformation@gmail.com>
2013-06-30 12:52:58 -0400

    v0.1.4 installer
    installer converted to be more generic for other programs to use.

--- install_frenamer.sh
+++ install_frenamer.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 #Author: Jason Campisi
-#Date: 2013
-#version 0.1.3
+#Date: 6/30/2013
+#version 0.1.4
 #Released under the GPL v2 or higher
-
-echo "frenamer installer:";
+NAME="frenamer"
+EXT="pl"
+echo "$NAME installer:";
 
 echo " Setting file to executable...";
-chmod +x ./frenamer.pl
+chmod +x ./$NAME.$EXT
 
 echo " Checking for Perl at /usr/bin/perl ...";
 if [ "$(whereis perl)" != '/usr/bin/perl' ]; then
@@ -20,7 +21,7 @@ fi
 
 echo " Checking if you have the clearance to install this ...";
 if [ "$(whoami)" != 'root' ]; then
-	echo " You do not have permission to install 'frenamer.pl'";
+	echo " You do not have permission to install ./$NAME.$EXT";
 	echo " ->You must be a root user.";
 	echo " ->Try instead: sudo $0";
 	exit 1;
@@ -28,8 +29,8 @@ else
 	echo " Root access granted for $0";	
 fi
 
-echo " Installing to /usr/bin/ ...";
-cp ./frenamer.pl /usr/bin/frenamer
+echo " Installing $NAME to /usr/bin/ ...";
+cp ./$NAME.$EXT /usr/bin/$NAME
 echo " Setup complete."
-echo "Testing install with this command\n>frenamer -version";
-frenamer --version
+echo "Testing install with this command\n>$NAME --version";
+$NAME --version
