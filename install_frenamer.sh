ee40c1eaac707ef4801e00bb4e2892d28a7e2e68
xeoron <aitsinformation@gmail.com>
2013-07-14 13:27:00 -0400

    v0.2.3
    made code eaiser to make changes

--- install_frenamer.sh
+++ install_frenamer.sh
@@ -1,24 +1,25 @@
 #!/bin/sh -e
 #Author: Jason Campisi
 #Date: 6/30/2013
-#version 0.2.1
+#version 0.2.2
 #Released under the GPL v2 or higher
 NAME="frenamer"
 EXT="pl"
-echo "$NAME installer:";
+FILE="$NAME.$EXT"
+echo "$FILE installer:";
 
- echo " Checking if '$NAME.$EXT' exists in the current folder..."
-   if [ ! -n "$NAME.$EXT" ]; then
+ echo " Checking if '$FILE' exists in the current folder..."
+   if [ ! -n "$FILE" ]; then
       echo " $NAME.$EXT Error - File name is not set!"
       exit 1;
-   elif [ ! -e "$NAME.$EXT" ]; then
+   elif [ ! -e "$FILE" ]; then
       echo " Error - The location of '$NAME.$EXT' does not exist!"
       exit 1;
    fi
  echo " ...found!";
  
 echo " Setting file to executable...";
-chmod +x ./$NAME.$EXT
+chmod +x ./$FILE
 
 echo " Checking for Perl at /usr/bin/perl ...";
 if [ "$(whereis perl)" != '/usr/bin/perl' ]; then
@@ -26,12 +27,12 @@ if [ "$(whereis perl)" != '/usr/bin/perl' ]; then
 	echo " ->If it is installed, then make a symlink that points /usr/bin/perl to the right location, else please install it.";
 	exit 1;
 else 
-	echo " Perl found!";
+	echo " ...Perl found!";
 fi
 
 echo " Checking if you have the clearance to install this ...";
 if [ "$(whoami)" != 'root' ]; then
-	echo " You do not have permission to install ./$NAME.$EXT";
+	echo " You do not have permission to install ./$FILE";
 	echo " ->You must be a root user.";
 	echo " ->Try instead: sudo $0";
 	exit 1;
@@ -40,7 +41,7 @@ else
 fi
 
 echo " Installing $NAME to /usr/bin/ ...";
-cp ./$NAME.$EXT /usr/bin/$NAME
+cp ./$FILE /usr/bin/$NAME
 echo " Setup complete."
 echo "Testing install with this command\n>$NAME --version";
 $NAME --version
